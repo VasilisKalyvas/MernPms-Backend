3af174f358e42f56579462f65d62eda726a88be3
VasilisKalyvas <101932947+VasilisKalyvas@users.noreply.github.com>
2022-03-22 22:06:29 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const  mongoose  = require("mongoose")
 
 const connectDB = async () => {
     return mongoose
-    .connect("mongodb+srv://bkalivas:((u27SOx@cluster0.h3acx.mongodb.net/CMS?retryWrites=true&w=majority")
+    .connect("//mongodbURL")
     .then(()=> console.log(`Connected to the database...`))
     .catch((err) => console.log(err));
 };
